a4eecaca3b0dd5ce2988052afe7082f27b53a48c
Ronald Hong <ron@wistia.com>
2015-07-07 10:06:28 -0400

    Fix some stuff. (it works now!)

--- lib/daemon.rb
+++ lib/daemon.rb
@@ -1,17 +1,26 @@
+require 'logger'
+
 class Daemon
-  DEFAULT_POLL_EVERY = 1.minute
+  DEFAULT_POLL_EVERY = 60
+  DEFAULT_LOGGER = Logger.new(STDOUT)
 
   def initialize(source, endpoint, options = {})
     @source = source
     @endpoint = endpoint
-    @poll_every = options[:poll_every]
+    @poll_every = options[:poll_every] || DEFAULT_POLL_EVERY
+    @logger = options[:logger] || DEFAULT_LOGGER
   end
 
   def run!
     loop do
-      @last_polled_at = Time.now
-      Notifier.new(@source, @endpoint, since: @last_polled_at).notify!
-      sleep(@poll_every)
+      begin
+        @last_polled_at = Time.now
+        Notifier.new(@source, @endpoint).notify!(since: @last_polled_at)
+        @logger.info("[INFO] Polled at #{@last_polled_at}")
+        sleep(@poll_every)
+      rescue => e
+        @logger.error("[ERROR] #{e.class}: #{e.message}\n#{e.backtrace}")
+      end
     end
   end
 end
