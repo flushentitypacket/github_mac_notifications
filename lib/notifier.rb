3fc5f96ea2efeb0bd64eb431ed87e4c5abfb76bc
Ronald Hong <ron@wistia.com>
2016-01-22 11:08:05 -0500

    Add crude URL parsing to link directly to notification URL.

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -9,14 +9,25 @@ class Notifier
       message = notification.subject.title
       title = 'Github'
       subtitle = notification.subject.type
-      link = 'https://github.com/notifications'
+      link = html_url_from_api_url(notification.subject.url)
 
       @endpoint.notify(message, title: title, subtitle: subtitle, open: link)
     end
   end
 
+  private
+
   def notifications_since(t = Time.at(0))
     # `@source.notifications` gives unread only by default
     @source.notifications(since: t)
+  end
+
+  def html_url_from_api_url(api_url)
+    uri = URI.parse(api_url)
+    # format: "/repos/PROJECT/REPO/pulls/123"
+    uri.path = uri.path.split('/').
+      tap { |a| a[4].chomp!('s'); a.slice!(1) }.join('/')
+    uri.host = 'github.com'
+    uri.to_s
   end
 end
